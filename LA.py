e9f60d0dd994fb62dec6d602c99b7aac0020f482
FabianMuli <fabian.muli@students.jkuat.ac.ke>
2019-01-12 20:36:27 +0300

    added class

--- LA.py
+++ LA.py
@@ -3,20 +3,33 @@ import aiml
 import os
 import sys
 
+class LA():
+    def __init__(self):
+        self.kernel = aiml.Kernel()
 
-def main():
-    if __name__ == '__main__':
-        kernel = aiml.Kernel()
-
+    def initialize(self):
         if os.path.isfile("LABrain.brn"):
-            kernel.bootstrap(brainFile="LABrain.brn")
+            self.kernel.bootstrap(brainFile="LABrain.brn")
     
         else:
-            kernel.bootstrap(learnFiles="std.xml", commands="load aiml b")
-            kernel.saveBrain("LABrain.brn")
+            self.kernel.bootstrap(learnFiles="std.xml", commands="load aiml b")
+            self.kernel.saveBrain("LABrain.brn")
+
+        properties_file = open(os.sep.join(["LA/lib/system","rosie.properties"]))
+        for line in properties_file:
+            parts = line.split('=')
+            key = parts[0]
+            value = parts[1]
+            self.kernel.setBotPredicate(key,value)
 
         while True:
-           print(kernel.respond(raw_input("~> ")))
+            print(self.kernel.respond(raw_input("~> ")))
 
 
-main()
+def main():
+    la = LA()
+    la.initialize()
+
+if __name__ =='__main__':
+    main()
+        
